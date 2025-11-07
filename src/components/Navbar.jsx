f35d0b3b88ec36428f8ef5a80ebaf401ab91a0b2
Bryan Navarro <diazbnavarro@gmail.com>
2025-11-07 18:54:01 +0000

    logo added, will work on card component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ export const Navbar = () => {
 
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+			<div className="container d-flex justify-content-between">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src="https://i5.walmartimages.com/seo/Star-Wars-Logo-Picture-Cartoon-Character-Wall-Art-Vinyl-Sticker-Design-Decal-Girl-Boy-Kids-Bedroom-Nursery-Kindergarten-Fun-Home-Children-Room-Decor_97d371d2-00dd-4ed7-85cd-48e8099c3393_1.068cf2e7d751e8b137e88605bc3efadd.jpeg" alt="" style={{height: '50px', width: '50px'}} />
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					<header>Something to Add</header>
+					{/* <Link to="/demo">
+					</Link> */}
 				</div>
 			</div>
 		</nav>
